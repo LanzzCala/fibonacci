27dfc7f8959306b87abe489ba81d1dab18a6bdb3
Lanzz Cala <lanzzcala@gmail.com>
2025-04-08 18:40:22 -0700

    Adjust try block to include all of if else statement

--- fibonacci.py
+++ fibonacci.py
@@ -1,20 +1,23 @@
 def fib():
     try:
         number = int(input("Enter a number for the fibonacci sequence: "))
-        if number <= 1:
-            print ("You entered an invalid number")
+        if number == 0:
+            print(number)
+        elif number <= 1:
+            print (number)
+        else:
+            start = 0
+            first_num = 0
+            second_num = 1
+            end = number
+            while start != end:
+                print (first_num, end= " ")
+                result = first_num + second_num
+                second_num = first_num
+                first_num = result
+                start += 1
     except ValueError:
-        print("You entered words instead of a number. Please run the code again.")
-    else:
-        start = 0
-        first_num = 0
-        second_num = 1
-        end = number
-        while start != end:
-            print (first_num, end= " ")
-            result = first_num + second_num
-            second_num = first_num
-            first_num = result
-            start += 1
+        print(f"You entered text instead of a number. Please run the code again.")
+
 
 fib()
